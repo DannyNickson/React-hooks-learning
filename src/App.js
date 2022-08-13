788a1e2d3f3a6f6d3aed99ce90d9902b30e8af25
anny <puchkovdd@gmail.com>
2022-08-13 15:26:12 +0300

    added hoverhook

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import React from "react";
-import useInput from './hooks/useInput';
+// import useInput from './hooks/useInput';
+import Hover from './components/Hover';
 
 function App() {
 
-  const username=useInput('')
-  const password=useInput('')
-  
+  // const username=useInput('')
+  // const password=useInput('')
+  /* <input type="text" placeholder="'username" {...username}  />
+      <input type="text" placeholder="password"  {...password}/>
+      <button onClick={()=>{console.log(username.value,password.value)}}>Click</button> */
+      // <Hover/>
 
   return (
     <div>
-      <input type="text" placeholder="'username" {...username}  />
-      <input type="text" placeholder="password"  {...password}/>
-      <button onClick={()=>{console.log(username.value,password.value)}}>Click</button>
+
     </div>
   );
 }
