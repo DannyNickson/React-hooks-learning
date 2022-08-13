e926d0b5f1a9c2aef375a3cd167cf51821a62e92
anny <puchkovdd@gmail.com>
2022-08-13 15:06:08 +0300

    controlled input

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [value,setValue] = useState('')
+
+  const onChange = e=>{
+    setValue(e.target.value)
+  }
+
   return (
     <div>
-      
+      <input type="text" value={value} onChange={onChange} />
+      <button onClick={()=>{console.log(value)}}>Click</button>
     </div>
   );
 }
