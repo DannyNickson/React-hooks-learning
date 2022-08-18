f4e62a9bf8afef75165c0e32693bd83fe368e2d5
anny <puchkovdd@gmail.com>
2022-08-18 14:37:13 +0300

    new hook usedebounce

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import List from "./components/List";
 // import useInput from './hooks/useInput';
 // import Hover from './components/Hover';
+import useDebounce from './hooks/useDebounce';
 
 function App() {
 
@@ -12,10 +13,26 @@ function App() {
       <button onClick={()=>{console.log(username.value,password.value)}}>Click</button> */
   // <Hover />
   // <List />
+  const [value,setValue] = useState('')
+  const debouncedSearch = useDebounce(search,500)
+  function search(query) {
+    fetch(
+      `https://jsonplaceholder.typicode.com/todos?query=`+query
+    )
+      .then((response) => response.json())
+      .then(json => {
+        console.log(json);
+      })
+  }
+   const onChange =e=>{
+    setValue(e.target.value)
+    debouncedSearch(e.target.value)
+   }
+
 
   return (
     <div>
-      
+      <input type="text" value={value} onChange={onChange} />
     </div>
   );
 }
